9965280f580de041f9fcdc669b8f4b46086f6d2e
Haneen AbuKhater and Jessica Sheridan <haneen@>
2017-07-17 15:06:05 -0700

    Remove selected item

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 //Backend
 
-function Task(name) {
+function Task(name,date,location) {
   this.taskName = name;
+  this.dueDate = date;
+  this.location = location;
 };
 
 
@@ -12,20 +14,19 @@ $(document).ready(function() {
     var inputtedTask = $("input#new-task").val();
 
     var newTask = new Task(inputtedTask);
-    $(".checkbox").append("<input type='checkbox' name='task' value='" + newTask.taskName + "'>" + newTask.taskName + "<br>");
+    $(".checkbox").append("<li><input type='checkbox' name='task' value='" + newTask.taskName + "'>" + "<span id = '" + newTask.taskName + "'>" + newTask.taskName + "</span> <br>");
     $(".checklist").show();
 
+    $("input:text").val("");
   });
   $("form.checklist").submit(function(event){
     event.preventDefault();
-
-    var values = [];
-    $("input:checkbox:checked").each(function() {
-        values.push($(this).val());
+    var test = confirm("Are you sure you want to delete these tasks?");
+    if (test) {
+      $("input:checkbox:checked").each(function() {
+        $("input:checkbox:checked").parent().remove();
       });
-    alert(values);
-
-
+    }
   });
 
 
